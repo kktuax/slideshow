8681325366bd70b6ea306a80480f30b9172a125f
tunim <max@predictia.es>
2018-10-07 14:12:00 +0200

    look recursively into subfolders

--- src/main/java/com/gmail/maxilandia/slideshow/DisplayRunner.java
+++ src/main/java/com/gmail/maxilandia/slideshow/DisplayRunner.java
@@ -3,13 +3,16 @@ package com.gmail.maxilandia.slideshow;
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.nio.file.Files;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,19 +25,24 @@ import org.tw.pi.framebuffer.FrameBuffer;
 @Component
 public class DisplayRunner implements CommandLineRunner {
 
+	private final Random rand = new Random();
+	
+	private final ExecutorService es = Executors.newSingleThreadExecutor();
+	
 	@Override
 	public void run(String... args) throws Exception {
-		Random rand = new Random();
-		File[] files = folder.listFiles(f -> f.getName().toLowerCase().endsWith(".jpg"));
-		LOGGER.info(String.format("Found %s images in %s", files.length, folder));
 		NarSystem.loadLibrary();
 		FrameBuffer frameBuffer = new FrameBuffer(fbLocation);
-		ExecutorService es = Executors.newSingleThreadExecutor();
+		List<File> imageFiles = Files
+			.find(folder.toPath(), Integer.MAX_VALUE, (p, at) -> p.toFile().getName().toLowerCase().endsWith(".jpg"))
+			.map(p -> p.toFile())
+			.collect(Collectors.toList());
+		LOGGER.info(String.format("Found %s images in %s", imageFiles.size(), folder));
 		try{
 			BufferedImage screen = frameBuffer.getScreen();
 			Dimension boundary = new Dimension(screen.getWidth(), screen.getHeight());
 			LOGGER.info(String.format("Found resolution of %sx%s", screen.getWidth(), screen.getHeight()));
-			Future<Image> fi = es.submit(new ImageLoader(files[rand.nextInt(files.length)], boundary));
+			Future<Image> fi = es.submit(new ImageLoader(imageFiles.get(rand.nextInt(imageFiles.size())), boundary));
 			while (true) {
 				boolean displayNextImage = false;
 				if(fi.isDone()){
@@ -49,9 +57,9 @@ public class DisplayRunner implements CommandLineRunner {
 						fi.get().display(screen);
 						lastDisplayedTime = Instant.now();
 					}catch(Exception e){
-						LOGGER.warn(String.format("Error reading image: %s", e.getMessage()));
+						LOGGER.warn("Error reading image", e);
 					}
-					fi = es.submit(new ImageLoader(files[rand.nextInt(files.length)], boundary));
+					fi = es.submit(new ImageLoader(imageFiles.get(rand.nextInt(imageFiles.size())), boundary));
 				}				
 			}
 		}finally{
